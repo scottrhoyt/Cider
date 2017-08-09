ff20e3075f4fd74ba0da03fcfb8ce1b0f93a339a
Scott Hoyt <scottrhoyt@gmail.com>
2017-08-09 14:37:31 -0700

    Add AppleMusicAPIError

---
+++ Cider/CoreObjects/AppleMusicAPIError.swift
@@ -0,0 +1,20 @@
+//
+//  AppleMusicAPIError.swift
+//  Cider
+//
+//  Created by Scott Hoyt on 8/9/17.
+//  Copyright © 2017 Scott Hoyt. All rights reserved.
+//
+
+import Foundation
+
+struct AppleMusicAPIError: Error, Codable {
+    let id: String
+    let about: String
+    let status: String
+    let code: String
+    let title: String
+    let detail: String
+    // let source: Source
+    // let meta: Meta
+}
