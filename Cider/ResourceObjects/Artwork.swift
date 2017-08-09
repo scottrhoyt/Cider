d0a92bcd170431a4fea886934dbfdba69672fefe
Scott Hoyt <scottrhoyt@gmail.com>
2017-08-09 14:24:54 -0700

    Make Artwork conform to API Spec.

--- Cider/ResourceObjects/Artwork.swift
+++ Cider/ResourceObjects/Artwork.swift
@@ -9,16 +9,15 @@
 import Foundation
 
 public struct Artwork: Codable {
-    let bgColor: String
-    let height: Int
     let width: Int
+    let height: Int
     let url: String
+    let bgColor: String
 
-    // These might not be optionals
-    let textColor1: String?
-    let textColor2: String?
-    let textColor3: String?
-    let textColor4: String?
+    let textColor1: String
+    let textColor2: String
+    let textColor3: String
+    let textColor4: String
 
     public func url(forWidth width: Int) -> URL? {
         let urlString = url.replacingOccurrences(of: "{w}", with: "\(width)").replacingOccurrences(of: "{h}", with: "\(width)")
