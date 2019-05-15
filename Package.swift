47240b9bcf6ec867770f37e2a6921bc2e04e40b0
HackingGate <i@hackinggate.com>
2019-05-15 18:03:52 +0900

    Update to Swift 5

--- Package.swift
+++ Package.swift
@@ -1,5 +1,21 @@
+// swift-tools-version:5.0
+
 import PackageDescription
 
 let package = Package(
-    name: "Cider"
+    name: "Cider",
+    products: [
+        .library(
+            name: "Cider",
+            targets: ["Cider"]),
+    ],
+    targets:[
+        .target(
+            name: "Cider",
+            dependencies: [],
+            path: "Sources"),
+        .testTarget(
+            name: "CiderTests",
+            dependencies: ["Cider"]),
+    ]
 )
